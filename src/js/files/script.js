3a1eaed070f38f907698b83a46708ac0caec9806
D1m30 <petrenko_dv@ukr.net>
2023-02-22 13:44:05 +0200

    dct

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,7 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+import mixitup from 'mixitup';
+
+var mixer = mixitup('.block-order');
